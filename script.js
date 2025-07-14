b0215744e3b3010694d66ccbf4eca4d2b3cb2bc1
alisadeghitabar80 <aliseti2602@gmail.com>
2025-07-14 21:37:32 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 
-const containers = document.querySelectorAll(".image-container");
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.getElementById("gallery");
 
-containers.forEach(container => {
-  const img = container.querySelector(".zoom-img");
-  const magnifier = container.querySelector(".magnifier");
+  for (let i = 1; i <= 93; i++) {
+    const container = document.createElement("div");
+    container.className = "page";
 
-  container.addEventListener("mousemove", e => {
-    const rect = img.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+    const img = document.createElement("img");
+    img.src = `pages/page${i}.jpg`;
+    img.alt = `Page ${i}`;
 
-    const zoomLevel = 2;
-    magnifier.style.backgroundImage = `url('${img.src}')`;
-    magnifier.style.backgroundSize = `${img.width * zoomLevel}px ${img.height * zoomLevel}px`;
-    magnifier.style.backgroundPosition = `-${x * zoomLevel - 75}px -${y * zoomLevel - 75}px`;
-    magnifier.style.left = `${x - 75}px`;
-    magnifier.style.top = `${y - 75}px`;
-    magnifier.style.display = "block";
-  });
+    const lens = document.createElement("div");
+    lens.className = "lens";
 
-  container.addEventListener("mouseleave", () => {
-    magnifier.style.display = "none";
-  });
+    container.appendChild(img);
+    container.appendChild(lens);
+    gallery.appendChild(container);
+
+    container.addEventListener("mousemove", function (e) {
+      lens.style.display = "block";
+      const rect = img.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      lens.style.left = `${x - 75}px`;
+      lens.style.top = `${y - 75}px`;
+      lens.style.backgroundImage = `url(${img.src})`;
+      lens.style.backgroundRepeat = "no-repeat";
+      lens.style.backgroundSize = `${img.width * 2}px ${img.height * 2}px`;
+      lens.style.backgroundPosition = `-${x}px -${y}px`;
+    });
+
+    container.addEventListener("mouseleave", function () {
+      lens.style.display = "none";
+    });
+  }
 });
