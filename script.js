03458f3d7084f91d576cf998e77459520a57ba83
alisadeghitabar80 <aliseti2602@gmail.com>
2025-07-14 22:26:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelectorAll(".img-container").forEach(container => {
   const img = container.querySelector("img.zoomable");
   const lens = container.querySelector(".lens");
 
-  const zoom = 3;
+  const zoom = 2.5;
 
   img.addEventListener("load", () => {
     lens.style.backgroundImage = `url('${img.src}')`;
